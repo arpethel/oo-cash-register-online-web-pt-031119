ad41e04e60d0faafa739584a0212f6460173502e
Ryan Pethel <arpethel@gmail.com>
2019-04-06 14:52:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-class CashRegister 
+class CashRegister
 
   attr_accessor :total
 
@@ -11,14 +11,14 @@ class CashRegister
 
   def total
     #returns current total
-    
+
   end
 
   def add_item(title, price)
     #increases the total price
     #also accepts an optional quantity
     #doesn't forget about the previous total
-    
+
   end
 
   def apply_discount
@@ -26,17 +26,17 @@ class CashRegister
     #the cash register was initialized with an employee discount returns success message with updated total
     #the cash register was initialized with an employee discount reduces the total
     #the cash register was not initialized with an employee discount returns a string error message that there is no discount to apply
-    
+
   end
 
   def items
     #returns an array containing all items that have been added
-    
+
   end
 
   def void_last_transaction
     #subtracts the last transaction from the total
-    
+
   end
 
 
