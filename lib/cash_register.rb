6099bd63ece7f04a240d1db72dc8cc3d8e75c600
Ryan Pethel <arpethel@gmail.com>
2019-04-06 15:53:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ class CashRegister
   attr_accessor :total, :title, :price, :discount
 
 
-  def initialize(total = 0, discount = nil)
+  def initialize(total = 0, discount*)
     @total = total
     @items = []
     @discount = discount.to_f/100.to_f
