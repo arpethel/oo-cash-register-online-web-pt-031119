27a98cb37fbc856a4dabb1ab8f3d017d8ffb5988
Ryan Pethel <arpethel@gmail.com>
2019-04-06 16:00:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ class CashRegister
   attr_accessor :total, :title, :price, :discount
 
 
-  def initialize(total = 0, discount = 0)
+  def initialize(total = 0, discount = nil)
     @total = total
     @items = []
     @discount = discount.to_f/100.to_f
@@ -34,7 +34,7 @@ class CashRegister
     #the cash register was not initialized with an employee discount returns a string error message that there is no discount to apply
     # binding.pry
     # binding.pry
-    if @discount != 0
+    if @discount != 0 || @discount != nil
       @discount *= @total
       @total -= @discount
       @total
